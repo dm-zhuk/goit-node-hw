1066f8f2e99daa36c543a16d525fbf75e0e314de
DmytroZhuk <dm.zhuk@gmail.com>
2024-07-16 19:38:10 +0300

    .env create

---
+++ simple-express/routes/index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/:id", function (req, res, next) {
+  const { id } = req.params;
+  const contact = contacts.filter((el) => el.id === id);
+  res.json(contact);
+});
+
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+  res.render("response", { title: "Simple express app", email, password });
+});
+
+module.exports = router;
